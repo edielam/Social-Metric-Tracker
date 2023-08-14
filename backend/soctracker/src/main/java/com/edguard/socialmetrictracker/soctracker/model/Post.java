111e709e525a6a58e4fc793917a8c80259aaba08
edielam <edguard738@gmail.com>
2023-08-14 21:26:11 +0000

    data model design

--- backend/soctracker/src/main/java/com/edguard/socialmetrictracker/soctracker/model/Post.java
+++ backend/soctracker/src/main/java/com/edguard/socialmetrictracker/soctracker/model/Post.java
@@ -36,6 +36,62 @@ public class Post {
     }
 
     // Getters and setters for all attributes
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    public int getShares() {
+        return shares;
+    }
+
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
 
 
