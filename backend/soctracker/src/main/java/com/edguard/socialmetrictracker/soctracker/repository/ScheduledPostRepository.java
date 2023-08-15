1dd971599edffd99496b090e5f9e875f67d1396a
edielam <edguard738@gmail.com>
2023-08-15 13:23:03 +0000

    reating repositories, extending jpaRepo

---
+++ backend/soctracker/src/main/java/com/edguard/socialmetrictracker/soctracker/repository/ScheduledPostRepository.java
@@ -0,0 +1,10 @@
+package com.edguard.socialmetrictracker.soctracker.repository;
+
+import com.edguard.socialmetrictracker.soctracker.model.ScheduledPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduledPostRepository extends JpaRepository<ScheduledPost, Long> {
+    // Additional query methods can be added here if needed
+}
